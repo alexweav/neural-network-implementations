8f6b70bc8356c72d6404b8f1ea7fcd087e0058a3
alexweav <alexander.weaver@ttu.edu>
2016-03-15 22:58:41 -0500

    Modified functions to not depend on MNIST data

--- LogisticNeuralNetwork/makePredictions.m
+++ LogisticNeuralNetwork/makePredictions.m
@@ -1,5 +1,4 @@
 function y = makePredictions(x, params)
   result = evaluateNetwork(x, params);
   [maxVals, y] = max(result, [], 2);
-  y(y == 10) = 0;
 end
--- LogisticNeuralNetwork/percentError.m
+++ LogisticNeuralNetwork/percentError.m
@@ -1,7 +1,6 @@
 function j = percentError(x, y, params)
   predictions = makePredictions(x, params);
   [dummy, indices] = max(y, [], 2);
-  indices(indices == 10) = 0;
   numErrors = size(indices(indices != predictions), 1);
   j = numErrors / size(x, 1);
 end
